108c49a87ad3ffc05d7433f1df2611029f1ae1fc
syaoran <1412360581@qq.com>
2026-05-15 21:26:22 +0800

    Site updated: 2026-05-15 21:26:19

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1778804422237";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1778851538871";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
